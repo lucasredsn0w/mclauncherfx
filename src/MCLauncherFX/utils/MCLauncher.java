68e5b2c42267640a5210885e911f2989d99458a5
Lucas <270436171@qq.com>
2013-08-17 21:19:23 +0800

    Added suggesting memory.

--- src/MCLauncherFX/utils/MCLauncher.java
+++ src/MCLauncherFX/utils/MCLauncher.java
@@ -1,7 +1,32 @@
 package MCLauncherFX.utils;
 import java.io.*;
 import java.awt.Graphics;
+import com.sun.management.OperatingSystemMXBean; 
+import sun.management.ManagementFactoryHelper;
+
+
+
 public class MCLauncher {
+    public static final int mb=1048576;
+    public static int GetProperMemory(){
+        OperatingSystemMXBean osmxb =  (OperatingSystemMXBean) ManagementFactoryHelper.getOperatingSystemMXBean(); 
+        long totalmem = osmxb.getFreePhysicalMemorySize()/mb;
+        
+                
+        if(totalmem<=512)
+            return 256;
+        else if(totalmem<=1024)
+            return 512;
+        else if(totalmem<=2048)
+            return 1024;
+        else if (totalmem<=4096)
+            return 2048;
+        return 1;
+        
+    }
+    
+        
+    
 	public static void start(String username,int maxmem,String javapath,boolean isjavaboot)throws IOException{
 		
 		
